13a5d9d6a19fc2c1392961114f680358b5f68140
Eshan <thisiseshan@gmail.com>
2019-06-19 06:04:33 +0400

    Add training file

---
+++ mnist-gan.py
@@ -0,0 +1,273 @@
+%matplotlib inline
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+import torchvision
+from torchvision import datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+# Initialize Batch Size
+batch_size = 64
+
+# number of subprocesses to use for data loading
+num_workers = 0
+
+transform = transforms.ToTensor()
+
+# Load Dataset
+train_dataset = datasets.MNIST(root='data', train=True, download =True, transform=transform)
+
+# Prepare the dataloader
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=batch_size, num_workers=num_workers )
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Discriminator(nn.Module):
+
+  def __init__(self, input_size, hidden_dim, output_size):
+    super(Discriminator, self).__init__()
+
+    # Layers
+    self.conv1 = nn.Linear(input_size, hidden_dim*4)    # Input Layer
+    self.conv2 = nn.Linear(hidden_dim*4, hidden_dim*2)
+    self.conv3 = nn.Linear(hidden_dim*2, hidden_dim)
+    self.conv4 = nn.Linear(hidden_dim, output_size)  # Final Layer
+
+    # Dropout layer
+    self.dropout = nn.Dropout(0.3)
+
+  def forward(self, x):
+    # Flatten image
+    x = x.view(-1, 28*28)
+
+    x = F.leaky_relu(self.conv1(x), 0.2)   # (input, negative_slope)
+    x = self.dropout(x)
+    x = F.leaky_relu(self.conv2(x), 0.2)
+    x = self.dropout(x)
+    x = F.leaky_relu(self.conv3(x), 0.2)
+    x = self.dropout(x)
+
+    # Final Layer
+    out = self.conv4(x)
+    return out
+
+
+class Generator(nn.Module):
+
+  def __init__(self, input_size, hidden_dim, output_size):
+    super(Generator, self).__init__()
+
+    # Layers
+    self.conv1 = nn.Linear(input_size, hidden_dim)    # Input Layer
+    self.conv2 = nn.Linear(hidden_dim, hidden_dim*2)
+    self.conv3 = nn.Linear(hidden_dim*2, hidden_dim*4)
+    self.conv4 = nn.Linear(hidden_dim*4, output_size)  # Final Layer
+
+    # Dropout layer
+    self.dropout = nn.Dropout(0.3)
+
+  def forward(self, x):
+    x = F.leaky_relu(self.conv1(x), 0.2)   # (input, negative_slope)
+    x = self.dropout(x)
+    x = F.leaky_relu(self.conv2(x), 0.2)
+    x = self.dropout(x)
+    x = F.leaky_relu(self.conv3(x), 0.2)
+    x = self.dropout(x)
+
+    # Final Layer
+    output = F.tanh(self.conv4(x))
+
+    return output
+
+
+# Discriminator Hyperarameters
+
+# Size of input image
+input_size = 784
+# Size of discriminator output
+d_output_size = 1
+# Size of last hidden layer in discriminator (layer 3)
+d_hidden_size = 32
+
+
+# Generator Hyperparamters
+
+# Size of latent vector to give to Generator (reffered as noise)
+z_size = 100
+# Size of discriminator output (generated image)
+g_output_size = 784
+# Size of first hidden layer
+g_hidden_size = 32
+
+
+D = Discriminator(input_size, d_hidden_size, d_output_size)
+G = Generator(z_size, g_hidden_size, g_output_size)
+
+print(D)
+print()
+print(G)
+
+
+# Calculating losses
+def real_loss(D_out, smooth=False):
+    batch_size = D_out.size(0)
+    # label smoothing
+    if smooth:
+        # smooth, real labels = 0.9
+        labels = torch.ones(batch_size)*0.9
+    else:
+        labels = torch.ones(batch_size) # real labels = 1
+
+    # numerically stable loss
+    criterion = nn.BCEWithLogitsLoss()
+    # calculate loss
+    loss = criterion(D_out.squeeze(), labels)
+    return loss
+
+def fake_loss(D_out):
+    batch_size = D_out.size(0)
+    labels = torch.zeros(batch_size) # fake labels = 0
+    criterion = nn.BCEWithLogitsLoss()
+    # calculate loss
+    loss = criterion(D_out.squeeze(), labels)
+    return loss
+
+
+import torch.optim as optim
+
+# Learning Rate
+lr = 0.002
+
+# Optimizers
+d_optimizer = optim.Adam(D.parameters(), lr)
+g_optimizer = optim.Adam(G.parameters(), lr)
+
+
+
+import pickle as pkl
+
+# Defining Epochs
+num_epochs = 100
+
+# To keep track of loss and generated "fake" images
+samples = []
+losses = []
+
+print_every = 400
+
+# Getting fixed data fro sampling (constant unaltered throughtout training, allows to inspect the model's performance)
+
+sample_size = 16
+fixed_z = np.random.uniform(-1, 1, size=(sample_size, z_size))
+fixed_z = torch.from_numpy(fixed_z).float()
+
+# train the network
+D.train()
+G.train()
+for epoch in range(num_epochs):
+
+  for batch_i, (real_images, _) in enumerate(train_loader):
+
+    batch_size = real_images.size(0)
+
+
+    # Rescale Images from [0,1) to [-1, 1]
+    real_images = real_images*2 - 1
+
+
+    # TRAINING DISCRIMINATOR
+    d_optimizer.zero_grad()
+
+
+    # 1. Train with Real Images
+
+    # Compute the discriminator loss on real images
+    # Smooth the real labels
+    D_real = D(real_images)
+    d_real_loss = real_loss(D_real, smooth=True)
+
+
+    # 2. Train with fake images
+
+
+    # Generate fake images
+    z = np.random.uniform(-1, 1, size =(batch_size, z_size))
+    z = torch.from_numpy(z).float()
+    fake_images = G(z)
+
+    # Compute the discriminator losses on fake images
+    D_fake = D(fake_images)
+    d_fake_loss = fake_loss(D_fake)
+
+    # Adding up fake losses and perform backpropagation
+    d_loss = d_real_loss + d_fake_loss
+    d_loss.backward()
+    d_optimizer.step()
+
+
+    # TRAINING GENERATOR
+    g_optimizer.zero_grad()
+
+
+    # 1. Train with fake images and flipped labels
+
+
+    # Generate fake labels
+    z = np.random.uniform(-1, 1, size=(batch_size, z_size))
+    z = torch.from_numpy(z).float()
+    fake_images = G(z)
+
+    # Compute discriminator losses on fake images using flipped labels
+    D_fake = D(fake_images)
+    g_loss = real_loss(D_fake)    # Using real loss to flip labels
+
+    # Perform backprop
+    g_loss.backward()
+    g_optimizer.step()
+
+    # Printing some loss stats
+    if batch_i % print_every == 0:
+      # Print discriminator and generator loss
+      print('Epoch [{:5d}/{:5d}] | d_loss: {:6.4f} | g_loss: {:6.4f}'.format(epoch+1, num_epochs, d_loss.item(), g_loss.item()))
+
+
+    # After each Epoch
+    # Append discriminator and generator loss
+    losses.append((d_loss.item(), g_loss.item()))
+
+    # Generate and save sample, fake images
+    G.eval()  # Evaluate mode for generating samples
+    samples_z = G(fixed_z)
+    samples.append(samples_z)
+    G.train()   # Back to train code
+
+
+
+# Save training generator samples
+with open('train_samples.pkl', 'wb') as f:
+  pkl.dump(samples, f)
+
+
+
+fig, ax = plt.subplots()
+losses = np.array(losses)
+plt.plot(losses.T[0], label='Discriminator')
+plt.plot(losses.T[1], label='Generator')
+plt.title("Training Losses")
+plt.legend()
+
+
+# Helper function for viewing a list of passed in sample images
+def view_samples(epoch, samples):
+    fig, axes = plt.subplots(figsize=(7,7), nrows=4, ncols=4, sharey=True, sharex=True)
+    for ax, img in zip(axes.flatten(), samples[epoch]):
+        img = img.detach()
+        ax.xaxis.set_visible(False)
+        ax.yaxis.set_visible(False)
+        im = ax.imshow(img.reshape((28,28)), cmap='Greys_r')
+
+# Load samples from generator, taken while training
+with open('train_samples.pkl', 'rb') as f:
+    samples = pkl.load(f)
